2002625610655edb1691eb5193c8098cf4bc57e8
mjtechworks <michaeljohnson00518@gmail.com>
2021-11-10 19:50:53 -0500

    Update server.go

--- server/server.go
+++ server/server.go
@@ -36,6 +36,23 @@ func main() {
     http.Handle("/", handlerWithLogging)
     http.ListenAndServe(":2000", nil)
 }
+
+func apiResponse(w http.ResponseWriter, r *http.Request) {
+  // Set the return Content-Type as JSON like before
+  w.Header().Set("Content-Type", "application/json")
+
+  // Change the response depending on the method being requested
+  switch r.Method {
+    case "GET":
+      w.WriteHeader(http.StatusOK)
+      w.Write([]byte(`{"message": "GET method requested"}`))
+    case "POST":
+        w.WriteHeader(http.StatusCreated)
+        w.Write([]byte(`{"message": "POST method requested"}`))
+    default:
+        w.Write([]byte(`{"message": "Can't find method requested"}`))
+    }
+}
     
 func middlewareOne(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -64,8 +81,9 @@ func final(w http.ResponseWriter, r *http.Request) {
 
 	finalHandler := http.HandlerFunc(final)
 	mux.Handle("/", middlewareOne(middlewareTwo(finalHandler)))
-
 	log.Println("Listening on :2000...")
 	err := http.ListenAndServe(":2000", mux)
 	log.Fatal(err)
+    http.HandleFunc("/",apiResponse)
+    log.Fatal(http.ListenAndServe(":2000",nil))
 }
